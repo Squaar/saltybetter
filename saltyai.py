424116791eec937842fc0148188c5b10055ed4e8
Matt Dumford <mdumford99@gmail.com>
2017-10-22 10:31:39 -0500

    did some debugging, added some comments

--- saltyai.py
+++ saltyai.py
@@ -8,7 +8,7 @@ class LogRegression:
 
     _ALPHA = 0.2
     
-    def __init__(self, b_bias=0, b_p1elo=0, b_p2elo=0, b_p1winsvp2=0, b_p2winsvp1=0, b_p1winpct=0, b_p2winpct=0):
+    def __init__(self, b_bias=0.0, b_p1elo=0.0, b_p2elo=0.0, b_p1winsvp2=0.0, b_p2winsvp1=0.0, b_p1winpct=0.0, b_p2winpct=0.0):
         self.b_bias = b_bias
         self.b_p1elo = b_p1elo
         self.b_p2elo = b_p2elo
@@ -21,33 +21,41 @@ class LogRegression:
     def p(self, p1elo, p2elo, p1winsvp2, p2winsvp1, p1winpct, p2winpct):
         linear = self.b_bias + self.b_p1elo*p1elo + self.b_p2elo*p2elo + self.b_p1winsvp2*p1winsvp2 + self.b_p2winsvp1*p2winsvp1
         linear += self.b_p1winpct*p1winpct + self.b_p2winpct*p2winpct
-        return 1.0 / (1.0 + exp(-linear))
+        logified = 1.0 / (1.0 + exp(-linear))
+        import pdb; pdb.set_trace()
+        return logified
 
-    def train(self, training_data, epochs=10):
+    def train(self, training_data, epochs=1):
         self.log_betas()
         for i in range(epochs):
             correct = 0
             for fight in training_data:
+                self.log_betas()
+                ##TODO: prediction always goes to 1.0 after first training instance, not updating betas
                 prediction = self.p(fight['p1elo'], fight['p2elo'], fight['p1winsvp2'], fight['p2winsvp1'], fight['p1winpct'], fight['p2winpct'])
                 if prediction > 0.5 and fight['winner'] == 2:
                     correct += 1
-                self.b_bias = self.recalc_beta(self.b_bias, fight['winner']-1, prediction, 1) # bias always has coefficient 1
-                self.b_p1elo = self.recalc_beta(self.b_p1elo, fight['winner']-1, prediction, fight['p1elo'])
-                self.b_p2elo = self.recalc_beta(self.b_p2elo, fight['winner']-1, prediction, fight['p2elo'])
-                self.b_p1winsvp2 = self.recalc_beta(self.b_p1winsvp2, fight['winner']-1, prediction, fight['p1winsvp2'])
-                self.b_p2winsvp1 = self.recalc_beta(self.b_p2winsvp1, fight['winner']-1, prediction, fight['p2winsvp1'])
-                self.b_p1winpct = self.recalc_beta(self.b_p1winpct, fight['winner']-1, prediction, fight['p1winpct'])
-                self.b_p2winpct = self.recalc_beta(self.b_p2winpct, fight['winner']-1, prediction, fight['p2winpct'])
+                import pdb; pdb.set_trace()
+                self.b_bias = self.recalc_beta(self.b_bias, fight['winner'], prediction, 1) # bias always has coefficient 1
+                self.b_p1elo = self.recalc_beta(self.b_p1elo, fight['winner'], prediction, fight['p1elo'])
+                self.b_p2elo = self.recalc_beta(self.b_p2elo, fight['winner'], prediction, fight['p2elo'])
+                self.b_p1winsvp2 = self.recalc_beta(self.b_p1winsvp2, fight['winner'], prediction, fight['p1winsvp2'])
+                self.b_p2winsvp1 = self.recalc_beta(self.b_p2winsvp1, fight['winner'], prediction, fight['p2winsvp1'])
+                self.b_p1winpct = self.recalc_beta(self.b_p1winpct, fight['winner'], prediction, fight['p1winpct'])
+                self.b_p2winpct = self.recalc_beta(self.b_p2winpct, fight['winner'], prediction, fight['p2winpct'])
+            shuffle(training_data)
             self.log_betas()
             log.info('Correct pct: %s' % (correct / len(training_data) * 100))
-            shuffle(training_data)
 
     # b: beta val to update
     # y: actual classification
     # prediction: current probability of y=1
     # x: coefficient for beta val to update
+    ##TODO: this is where it's broken... once prediction == 1, (1-prediction) == 0, so recalc will always = b and never change
     def recalc_beta(self, b, y, prediction, x):
-        return b + self._ALPHA * (y-prediction) * prediction * (1-prediction) * x
+        recalc =  b + self._ALPHA * (y-prediction) * prediction * (1-prediction) * x
+        import pdb;pdb.set_trace()
+        return recalc
    
     def log_betas(self):
         log.info('betas: bias: {bias}, p1elo: {p1elo}, p2elo: {p2elo}, p1winsvp2: {p1winsvp2}, p2winsvp1: {p2winsvp1}, p1winpct: {p1winpct}, p2winpct: {p2winpct}'.format(
